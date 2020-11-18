39e4db8626c567c228df309e44356ecd231d8f32
Fernando <fmti91@hotmail.com>
2020-11-18 19:39:16 -0300

    tip calculate

--- App.js
+++ App.js
@@ -22,10 +22,46 @@ const Input = styled.TextInput`
   background-color: #EEEEEE;
 `;
 
+const CalcButton = styled.Button`
+  margin-top: 10px;
+`;
+
+const ResultArea = styled.View`
+  width: 90%;
+  margin-top: 30px;
+  background-color: #EEEEEE;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ResultItemTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const ResultItem = styled.Text`
+  font-size: 16px;
+  margin-bottom: 30px;
+`;
+
 export default () => {
 
   //states
   const [ bill, setBill ] = useState('');
+  const [ tip, setTip ] = useState(0);
+
+  //functions
+  const calculate = () => {
+    let numericBill = parseFloat(bill); //convert to float
+
+      //validate
+      if (numericBill) {
+        setTip(numericBill * 0.1); //tip = 10%
+      } else {
+        alert("Enter account amount")
+      }
+  }
 
   return (
     <Page>
@@ -36,6 +72,20 @@ export default () => {
         value={bill} // bill value
         onChangeText={text=>setBill(text)}//change Input text
       />
+      <CalcButton title="Calculate" onPress={calculate} />
+      {tip > 0 &&
+        //show result area
+        <ResultArea>
+          <ResultItemTitle>Bill</ResultItemTitle>
+          <ResultItem>$ {parseFloat(bill).toFixed(2)}</ResultItem>
+
+          <ResultItemTitle>Tip</ResultItemTitle>
+          <ResultItem>$ {tip.toFixed(2)}</ResultItem>
+
+          <ResultItemTitle>Total</ResultItemTitle>
+          <ResultItem>$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
+        </ResultArea>
+      }
     </Page>
     );
 }
